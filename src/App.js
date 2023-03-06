a99459f9a3e01ff7a61017c08142364267321bc4
alexander.joey@gmail.com <learnacademy@LEARNs-MacBook-Air.local>
2023-03-06 14:20:24 -0800

    Grid layouy w/ question marks finished

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import "./App.css"
+import Square from "./components/Square.js"
 
 const App = () => {
   const [board, setBoard] = useState([
@@ -17,6 +18,17 @@ const App = () => {
   return (
     <>
       <h1>Treasure Hunt Game</h1>
+      <div className="gameboard">
+        {board.map((value, index) => {
+          return(
+            <Square
+            Key={index}
+            value={value}
+            /> 
+          ) 
+        })}
+        
+      </div>
     </>
   )
 }
